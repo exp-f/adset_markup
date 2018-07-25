fddceb1cda22e33b7630d0b051b82fdbc14a0223
artem@dobroagency.ru <cs1035idIII>
2018-07-25 23:48:32 +0400

    Можно редактировать всё

--- footer.php
+++ footer.php
@@ -69,6 +69,44 @@
 </div>
 </main>
 <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/js/index.js"></script>
+<!— Yandex.Metrika counter —>
+<script type="text/javascript">
+  (function (d, w, c) {
+    (w[c] = w[c] || []).push(function () {
+      try {
+        w.yaCounter49739467 = new Ya.Metrika2({
+          id: 49739467,
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true,
+          webvisor: true
+        });
+      } catch (e) {
+      }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+      s = d.createElement("script"),
+      f = function () {
+        n.parentNode.insertBefore(s, n);
+      };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://mc.yandex.ru/metrika/tag.js";
+    ;
+
+    if (w.opera == "[object Opera]") {
+      d.addEventListener("DOMContentLoaded", f, false);
+    } else {
+      f();
+    }
+  })(document, window, "yandex_metrika_callbacks2");
+</script>
+<noscript>
+    <div><img src="https://mc.yandex.ru/watch/49739467" ; style="position:absolute; left:-9999px;" alt=""/></div>
+</noscript>
+<!— /Yandex.Metrika counter —>
 <? wp_footer(); ?>
+
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -28,5 +28,66 @@ function get_features()
     return ob_get_clean();
 }
 
+function main_title()
+{
+    ob_start();
+    ?>
+    <h2><?=get_field('main_title')?></h2>
+    <?
+    return ob_get_clean();
+}
+
+function main_subtitle()
+{
+    ob_start();
+    ?>
+    <p><?=get_field('main_subtitle')?></p>
+    <?
+    return ob_get_clean();
+}
+
+function get_email()
+{
+    ob_start();
+    ?>
+    <a class="block_4_mail" href="mailto:<?=get_field('email')?>"><?=get_field('email')?></a>
+    <?
+    return ob_get_clean();
+}
+
+function get_phone()
+{
+    ob_start();
+    ?>
+    <a class="block_4_phone" href="mailto:<?=get_field('phone')?>"><?=get_field('phone')?></a>
+    <?
+    return ob_get_clean();
+}
+function get_address()
+{
+    ob_start();
+    ?>
+    <p class="block_4_address"><?=get_field('address')?></p>
+    <?
+    return ob_get_clean();
+}
+function get_disclaimer()
+{
+    ob_start();
+    ?>
+    <p><?=get_field('disclaimer')?></p>
+    <?
+    return ob_get_clean();
+}
+
+
+
+
 
 add_shortcode('features', 'get_features');
+add_shortcode('main_title', 'main_title');
+add_shortcode('main_subtitle', 'main_subtitle');
+add_shortcode('email', 'get_email');
+add_shortcode('phone', 'get_phone');
+add_shortcode('address', 'get_address');
+add_shortcode('disclaimer', 'get_disclaimer');
